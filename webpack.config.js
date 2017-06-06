d6c4c42e5938f9cf2ea1b9fb128891c13f7dda5d
jphager2 <jphager2@gmail.com>
2017-06-06 09:53:02 +0200

    update build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [ 'css-loader' ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    })
+  ]
+};
