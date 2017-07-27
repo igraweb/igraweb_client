0d1cdafef542eb711472023b2e293e32fb12affd
jphager2 <jphager2@gmail.com>
2017-07-27 16:40:59 +0200

    document router

--- source/router.js
+++ source/router.js
@@ -1,38 +1,84 @@
 /*jshint esversion: 6 */
 
-// TODO: Maybe "router" and "navigator" can be combined
-
 import config from './config';
 
 import navigator from './utils/navigator';
 
 const LoginModal = config.components.LoginModal;
 
+/**
+ * TODO: Maybe "router" and "navigator" can be combined
+ *
+ * router is responsible for opening replacing the page context, either by
+ * changing the window location or by opening a modal. Router is also used
+ * to query if the current context is at a named route. (See
+ * `config.routes`.)
+ */
 var router = {
+  /**
+   * @property paths
+   * @private
+   */
   paths: config.routes,
 
+  /**
+   * @property navigator
+   * @private
+   */
   navigator,
 
+  /**
+   * Reload the page.
+   *
+   * @method reload
+   * @public
+   */
   reload() {
     this.navigator.reload();
   },
 
+  /**
+   * Redirect to the notFound path.
+   *
+   * @method notFound
+   * @public
+   */
   notFound() {
     this.navigator.replace(this.paths.notFound);
   },
 
+  /**
+   * Load the login modal.
+   *
+   * @method unauthorized
+   * @public
+   */
   unauthorized() {
     var loginModal = LoginModal();
 
     loginModal.init();
   },
 
+  /**
+   * Change the page context (`window.location`) to a named path or a
+   * specified path.
+   *
+   * @method redirect
+   * @public
+   */
   redirect(pathOrPathName) {
     var path = this.path(pathOrPathName);
 
     this.navigator.replace(path);
   },
 
+  /**
+   * Check if the current page context (`window.location`) is at a named
+   * path or a specified path.
+   *
+   * @method path
+   * @private
+   */
   isAt(pathOrPathName) {
     var path = this.path(pathOrPathName);
 
@@ -40,13 +86,12 @@ var router = {
   },
 
   /**
+   * @method path
    * @private
-   * @method
    */
   path(pathOrPathName) {
     return this.paths[pathOrPathName] || pathOrPathName;
   },
-
 };
 
 export default router; 
