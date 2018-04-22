727e6b6080f017e90c7055028ad91488ceb5ff6b
jphager2 <jphager2@gmail.com>
2018-04-22 21:17:12 +0200

    Add chai for assertions

--- test/router_test.js
+++ test/router_test.js
@@ -2,113 +2,158 @@
 
 require('jsdom-global')();
 
-const assert = require('assert');
+const {expect} = require('chai');
 
 const config = require('../dist/config').default;
 const router = require('../dist/router').default;
 
 describe('router', () => {
-  it('redirects to a path', () => {
-    const calls = [];
+  describe('redirect', () => {
     const path = '/oh/a/path';
-    const originalNavigator = router.navigator;
+    let calls;
+    let originalNavigator;
 
-    router.navigator = Object.assign({}, originalNavigator, {
-      replace(href) {
-        calls.push(href);
-      }
+    before(() => {
+      calls = [];
+      originalNavigator = router.navigator;
+
+      router.navigator = Object.assign({}, originalNavigator, {
+        replace(href) {
+          calls.push(href);
+        }
+      });
     });
 
-    router.redirect(path);
-    router.navigator = originalNavigator;
+    after(() => {
+      router.navigator = originalNavigator;
+    });
 
-    assert.deepEqual(calls, [config.mount + path], 'router should redirect to the configured path');
+    it('redirects to a path', () => {
+      router.redirect(path);
+
+      expect(calls).to.deep.equal(
+        [config.mount + path],
+        'router should redirect to the configured path'
+      );
+    });
   });
 
-  it('redirects to a path outside of mount', () => {
-    const oldMount = config.mount;
-    const testMount = '/blah-blah-testing-mount';
+  describe('redirect with mount: false', () => {
     const path = '/te/st/in/g';
-    const calls = [];
-    const originalNavigator = router.navigator;
+    let calls;
+    let oldMount;
+    let testMount;
+    let originalNavigator;
 
-    config.mount = testMount;
+    before(() => {
+      calls = [];
+      oldMount = config.mount;
+      testMount = '/blah-blah-testing-mount';
+      originalNavigator = router.navigator;
 
-    router.navigator = Object.assign({}, originalNavigator, {
-      replace(href) {
-        calls.push(href);
+      config.mount = testMount;
+
+      router.navigator = Object.assign({}, originalNavigator, {
+        replace(href) {
+          calls.push(href);
+        }
+      });
+    });
+
+    after(() => {
+      config.mount = oldMount;
+      router.navigator = originalNavigator;
+    });
+
+    it('redirects to a path outside of mount', () => {
+      router.redirect(path, { mount: false });
+
+      expect(calls).to.deep.equal(
+        [path],
+        'router should redirect to path without mount'
+      );
+    });
+  });
+
+  describe('paths', () => {
+    const path = '/unexpected/path';
+    let originalNavigator;
+    let originalPath;
+    let calls;
+
+    before(() => {
+      calls = [];
+      originalNavigator = router.navigator;
+      originalPath = router.paths.notFound;
+
+      router.paths.notFound = path;
+
+      router.navigator = {
+        replace(href) {
+          calls.push(href);
+        }
+      };
+    });
+
+    after(() => {
+      router.paths.notFound = originalPath;
+      router.navigator = originalNavigator;
+    });
+
+    it('redirects to the named path', () => {
+      router.notFound();
+
+      expect(calls).to.deep.equal(
+        [path],
+        'router should redirect to the configured path'
+      );
+    });
+  });
+
+  describe('unauthorized', () => {
+    after(() => {
+      const modalNode = document.getElementById('igraweb-modal');
+
+      if (modalNode) {
+        modalNode.parentNode.removeChild(modalNode);
       }
     });
 
-    router.redirect(path, { mount: false });
-    config.mount = oldMount;
-    router.navigator = originalNavigator;
+    it('opens the login modal', () => {
+      let modalNode;
 
-    assert.deepEqual(calls, [path], 'router should redirect to path without mount');
+      modalNode = document.getElementById('igraweb-modal');
+
+      expect(modalNode).to.be.null;
+
+      router.unauthorized();
+
+      modalNode = document.getElementById('igraweb-modal');
+
+      expect(modalNode).not.to.be.null;
+      expect(modalNode.innerHTML).to.include('Login');
+      expect(modalNode.innerHTML).to.include('LOGIN');
+      expect(modalNode.querySelector('input[name="access_key"]')).not.to.be.null;
+      expect(modalNode.querySelector('input[name="secret_key"]')).not.to.be.null;
+    });
   });
 
-  it('redirects to the named path', () => {
-    const calls = [];
-    const path = '/unexpected/path';
-    const originalNavigator = router.navigator;
-    const originalPath = router.paths.notFound;
-
-    router.paths.notFound = path;
-
-    router.navigator = {
-      replace(href) {
-        calls.push(href);
-      }
-    };
-
-    router.notFound();
-    router.paths.notFound = originalPath;
-    router.navigator = originalNavigator;
-
-    assert.deepEqual(calls, [path], 'router should redirect to the configured path');
-  });
-
-  it('sanity', () => {
-    document.body.insertAdjacentHTML('beforeend', '<div id="test"></div>');
-    document.getElementById('test').innerHTML = '<p></p>';
-    assert(document.getElementById('test').innerHTML.includes('p'));
-  });
-
-  it('unauthorized route', () => {
-    let modalNode;
-
-    modalNode = document.getElementById('igraweb-modal');
-
-    assert(!modalNode);
-
-    router.unauthorized();
-
-    modalNode = document.getElementById('igraweb-modal');
-
-    assert(modalNode);
-    assert(modalNode.innerHTML.includes('Login'));
-    assert(modalNode.querySelector('input[name="access_key"]'));
-    assert(modalNode.querySelector('input[name="secret_key"]'));
-    assert(modalNode.innerHTML.includes('LOGIN'));
-
-    modalNode.parentNode.removeChild(modalNode);
-  });
-
-  /**
-   * describe('user_is_logged_in_when_login_button_is_clicked', () => {
-   * }),
-   */
-
-  it('can tell if the browser is at a path', () => {
+  describe('isAt', () => {
     const path = window.location.pathname;
 
-    router.paths.myTestPath = path;
+    before(() => {
+      router.paths.myTestPath = path;
+    });
 
-    assert(router.isAt(path));
-    assert(router.isAt('myTestPath'));
-    assert(!router.isAt('myUnexpectedPath'));
-    assert(!router.isAt('/my/Unexpected/Path'));
+    after(() => {
+      delete router.paths.myTestPath;
+    });
 
-    delete router.paths.myTestPath;
+    it('can tell if the browser is at a path', () => {
+      expect(router.isAt(path)).to.be.ok;
+      expect(router.isAt('myTestPath')).to.be.ok;
+      expect(!router.isAt('myUnexpectedPath')).to.be.ok;
+      expect(!router.isAt('/my/Unexpected/Path')).to.be.ok;
+    });
   });
 });
